ffb02565e41c0f8aefa3db6c4a9ff847a76f4829
gifcha <rudolfsbriedis77@gmail.com>
2025-03-04 14:31:26 +0200

    changed angular version to 19.2.0

--- task-manager/src/app/app.config.ts
+++ task-manager/src/app/app.config.ts
@@ -1,10 +1,9 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
 };
