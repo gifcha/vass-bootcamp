77a1cdf21e7bf992e404b13728dbd12de587d508
gifcha <rudolfsbriedis77@gmail.com>
2025-04-04 11:23:44 +0300

    Created routes and Improved the frontend design a little

--- task-manager/src/app/app.routes.ts
+++ task-manager/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TaskCreateComponent } from './task-create/task-create.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { TaskDetailsComponent } from './task-details/task-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'task-create', component: TaskCreateComponent },
+  { path: 'task-list', component: TaskListComponent },
+  { path: 'task-details', component: TaskDetailsComponent },
+];
