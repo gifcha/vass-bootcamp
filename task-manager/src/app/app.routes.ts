8e6295929c2b2f699745f08cf611ad09adbea97a
gifcha <rudolfsbriedis77@gmail.com>
2025-04-06 22:23:00 +0300

    task creating is now in a dialog window

--- task-manager/src/app/app.routes.ts
+++ task-manager/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import { TaskCreateComponent } from './task-create/task-create.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailsComponent } from './task-details/task-details.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
-  { path: 'task-create', component: TaskCreateComponent },
   { path: 'task-list', component: TaskListComponent },
+  {
+    path: 'task-create',
+    component: TaskListComponent
+
+  },
   { path: 'task-details', component: TaskDetailsComponent },
+  {
+    path: '',
+    redirectTo: 'task-list',
+    pathMatch: 'full'
+  },
 ];
--- task-manager/src/app/task-list/task-list.component.ts
+++ task-manager/src/app/task-list/task-list.component.ts
@@ -4,10 +4,18 @@ import { TaskCreateComponent } from '../task-create/task-create.component';
 import { TaskService } from '../task.service';
 import { MatCardModule } from '@angular/material/card';
 import { Task } from '../task.model';
+import { MatButtonModule } from '@angular/material/button';
+import { Dialog } from '@angular/cdk/dialog';
+import { Router } from '@angular/router';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-task-list',
-  imports: [MatListModule, TaskCreateComponent, MatCardModule],
+  imports: [
+    MatListModule,
+    MatCardModule,
+    MatButtonModule
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
@@ -17,11 +25,23 @@ export class TaskListComponent {
   taskList: Task[] = [];
   taskService = inject(TaskService);
 
-  constructor() {
+  private dialog = inject(Dialog);
+
+  constructor(private router: Router) {
     this.taskList = this.taskService.getTaskList();
+    if (this.router.url === '/task-create') {
+      this.openCreateTask();
+    }
   }
 
   removeTaskById(id: number) {
     this.taskService.removeTaskById(id);
+  }
+
+  openCreateTask() {
+      let d = this.dialog.open(TaskCreateComponent);
+      if (d.componentRef) {
+          d.componentRef.instance.dialog = d;
+      }
   }
 }
